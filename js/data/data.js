28db81bf9f8b6003e9e8e11950da8b8873a826eb
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-10-08 07:31:03 +0300

    Created javascript logic for progress bar rendering and animation

---
+++ js/data/data.js
@@ -0,0 +1,60 @@
+const data = [
+    {
+    pb1: 30,
+    pb2: 0,
+    pb3: 0,
+    pb4: 0,
+    label1: 'HTML/CSS',
+    label2: 'Javascript',
+    label3: 'Node.js',
+    label4: 'React',
+    icon1: '<i class="fab fa-html5"></i>',
+    icon2: '<i class="fab fa-js-square"></i>',
+    icon3: '<i class="fab fa-node-js"></i>',
+    icon4: '<i class="fab fa-react"></i>'
+    },
+    {
+    pb1: 70,
+    pb2: 40,
+    pb3: 0,
+    pb4: 0,
+    label1: 'HTML/CSS',
+    label2: 'Javascript',
+    label3: 'Node.js',
+    label4: 'React',
+    icon1: '<i class="fab fa-html5"></i>',
+    icon2: '<i class="fab fa-js-square"></i>',
+    icon3: '<i class="fab fa-node-js"></i>',
+    icon4: '<i class="fab fa-react"></i>'
+    },
+    {
+    pb1: 100,
+    pb2: 80,
+    pb3: 55,
+    pb4: 67,
+    label1: 'HTML/CSS',
+    label2: 'Javascript',
+    label3: 'Node.js',
+    label4: 'React',
+    icon1: '<i class="fab fa-html5"></i>',
+    icon2: '<i class="fab fa-js-square"></i>',
+    icon3: '<i class="fab fa-node-js"></i>',
+    icon4: '<i class="fab fa-react"></i>'
+    },
+    {
+    pb1: 100,
+    pb2: 100,
+    pb3: 100,
+    pb4: 100,
+    label1: 'HTML/CSS',
+    label2: 'Javascript',
+    label3: 'Node.js',
+    label4: 'React',
+    icon1: '<i class="fab fa-html5"></i>',
+    icon2: '<i class="fab fa-js-square"></i>',
+    icon3: '<i class="fab fa-node-js"></i>',
+    icon4: '<i class="fab fa-react"></i>'
+    }
+]
+
+export { data };
--- js/frontend.js
+++ js/frontend.js
@@ -1,36 +1,85 @@
+import { data } from "./data/data.js";
+const pb1 = 70;
+console.log(data);
 
 let progressBarAnimationDOM = document.querySelector('style');
-function renderProgressBar() {
+let progressBarDOM = document.querySelector('.progress-bar');
+function renderProgressBar(i) {
     setTimeout(() => {
-        progressBarAnimationDOM.innerHTML = `
-        @keyframes left{
-            100%{
-              transform: rotate(180deg);    // Atitinka 50% desiniojo rutulio
-            }
-          }
-        
-          @keyframes right{
-            100%{
-              transform: rotate(180deg);    // Atitinka 50% kairiojo rutulio
-            }
-          }
-        `
+        for (let j = 1; j <= 4; j++) {
+            progressBarAnimationDOM.innerHTML += `
+                .progress-bar${j} .circle .left .progress{
+                    z-index:1;
+                    animation: left${j} .4s linear both;
+                    animation-delay:.4s;
+                }
+                @keyframes left${j}{
+                    100%{
+                    transform: rotate(${(data[i]['pb' + j] * 3.6) > 180 ? (data[i]['pb' + j] * 3.6 - 180) : 0}deg);    // Atitinka 50% desiniojo rutulio
+                    }
+                }
+                .progress-bar${j} .circle .right .progress{
+                animation: right${j} .4s linear both;
+                }
+                @keyframes right${j}{
+                100%{
+                    transform: rotate(${(data[i]['pb' + j] * 3.6) < 180 ? (data[i]['pb' + j] * 3.6) : 180}deg);    // Atitinka 50% kairiojo rutulio
+                    }
+                }
+                `
+                progressBarDOM.innerHTML += `
+                    <div class="progress-bar${j}">
+                        <div class="circular">
+                        <div class="inner"></div>
+                        <div class="number"></div>
+                        <div class="circle">
+                        <div class="bar left">
+                            <div class="progress color${j}"></div>
+                        </div>
+                        <div class="bar right">
+                            <div class="progress color${j}"></div>
+                        </div>
+                        </div>
+                        </div>
+                        <div class="label1">${data[i]['label' + j]}</div>
+                        <div class="label2">Achievement: ${data[i]['pb' + j]}%</div>
+                    </div>
+                `
+        }
     }, 50);
+
+    setTimeout(() => {
+        const obj = document.querySelectorAll(".number");
+        obj.forEach((element, index) => {
+            counter(element, 0, data[i]['pb' + (index + 1)], 800, data[i]['icon' + (index + 1)]);
+
+        })
+    }, 55);
 }
 
-function clearProgressBar () {
+function clearProgressBar() {
     progressBarAnimationDOM.innerHTML = '';
+    progressBarDOM.innerHTML = '';
 }
 
 // NAVIGATION BUTTONS LISTENER
 const navigationDOM = document.querySelectorAll('.navigation li');
 navigationDOM.forEach((element, index) => {
     element.addEventListener('click', () => {
-        let html = `
-        
-        `
+
         clearProgressBar()
-        renderProgressBar();
+        renderProgressBar(index);
     })
 });
+
+// PERCENTAGE COUNTER
+function counter(obj, start, end, duration, icon) {
+    if (start <= end) {
+        setTimeout(() => {
+            obj.innerHTML = `${icon}<br>${start} %`;
+            console.log(start);
+            counter(obj, ++start, end, duration, icon);
+        }, duration / end);
+    }
+}
 
